83ddfb94514aeddf0f8ba3ddf9399f566740b9b9
dpatil1719 <patildhanashree888@gmail.com>
2025-10-04 18:43:40 +0530

    updated

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,32 +5,7 @@ import { MovieView } from "../movie-view/movie-view";
 export const MainView = () => {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
-  const [movies] = useState([
-    {
-      id: 1,
-      title: "Inception",
-      description: "A mind-bending thriller about dream invasion.",
-      genre: "Science Fiction",
-      director: "Christopher Nolan",
-      image: "https://image.tmdb.org/t/p/w500/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg"
-    },
-    {
-      id: 2,
-      title: "The Matrix",
-      description: "A hacker discovers the reality is a simulation.",
-      genre: "Science Fiction",
-      director: "The Wachowskis",
-      image: "https://m.media-amazon.com/images/I/51EG732BV3L._AC_.jpg"
-    },
-    {
-      id: 3,
-      title: "Interstellar",
-      description: "A journey through space and time to save humanity.",
-      genre: "Science Fiction",
-      director: "Christopher Nolan",
-      image: "https://m.media-amazon.com/images/I/91kFYg4fX3L._AC_UF894,1000_QL80_.jpg"
-    }
-  ]);
+  const [movies] = useState([]);
 
   if (selectedMovie) {
     return (
