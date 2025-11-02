f910658417f199129a82aa9530831ee7d4fb0388
dpatil1719 <patildhanashree888@gmail.com>
2025-11-02 13:07:30 +0530

    updated changes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,35 +1,52 @@
+// src/components/movie-view/movie-view.jsx
 import PropTypes from "prop-types";
+import Button from "react-bootstrap/Button";
+
 export const MovieView = ({ movie, onBackClick }) => {
-  if (!movie) {
-    return null;
-  }
+  if (!movie) return null;
 
   return (
     <div>
-      <h2>{movie.title}</h2>
+      <h2 className="h4 mb-3">{movie.title}</h2>
+
       {movie.image && (
         <img
           src={movie.image}
           alt={movie.title}
+          className="img-fluid mb-3"
         />
       )}
-      <p>{movie.description}</p>
-      {movie.genre && (
-        <p>
-          <strong>Genre:</strong> {movie.genre}
-        </p>
-      )}
-      {movie.director && (
-        <p>
-          <strong>Director:</strong> {movie.director}
-        </p>
-      )}
-      <button onClick={onBackClick}>Back</button>
+
+      {movie.description && <p className="mb-2">{movie.description}</p>}
+
+      <div className="mb-3">
+        {movie.genre && (
+          <div>
+            <strong>Genre:</strong> {movie.genre}
+          </div>
+        )}
+        {movie.director && (
+          <div>
+            <strong>Director:</strong> {movie.director}
+          </div>
+        )}
+      </div>
+
+      <Button variant="secondary" onClick={onBackClick}>
+        Back
+      </Button>
     </div>
   );
 };
 
 MovieView.propTypes = {
-  movie: MovieCard.propTypes.movie,   
+  movie: PropTypes.shape({
+    id: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    image: PropTypes.string,
+    genre: PropTypes.string,
+    director: PropTypes.string
+  }),
   onBackClick: PropTypes.func.isRequired
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,14 @@
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";   
 import "./index.scss";
 import { MainView } from "./components/main-view/main-view";
 
-const MyFlixApplication = () => {
-  return (
-    <div className="my-flix">
-      <MainView />
-    </div>
-  );
-};
+const MyFlixApplication = () => (
+  <div className="my-flix">
+    <MainView />
+  </div>
+);
 
-const container = document.querySelector("#root");
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<MyFlixApplication />);
